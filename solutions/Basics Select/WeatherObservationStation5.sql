549314f7e83aadd0d250c44ac23d87f708aff542
Aboubakr Jelloulat <abubakerjelloulat@gmail.com>
2025-09-23 17:23:13 +0100

    Weather Observation Station 5

---
+++ solutions/Basics Select/WeatherObservationStation5.sql
@@ -0,0 +1,21 @@
+/*
+
+    Query the two cities in STATION with the shortest and longest CITY names,
+        as well as their respective lengths (i.e.: number of characters in the name).
+        If there is more than one smallest or largest city, 
+        choose the one that comes first when ordered alphabetically.
+
+*/
+
+SET NOCOUNT ON;
+
+
+
+SELECT TOP 1 CITY, LEN(CITY) AS [LENGHT ] FROM STATION 
+    ORDER BY LEN(CITY) ASC, CITY ASC;
+
+
+SELECT TOP 1 CITY, LEN(CITY) AS [LENGHT] FROM STATION
+    ORDER BY LEN(CITY) DESC, CITY ASC;
+
+go
