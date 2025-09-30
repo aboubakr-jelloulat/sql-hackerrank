ccf40acb595ebaad421075812aff0e4bbf9278a7
aboubakr-jelloulat <abubakerjelloulat@gmail.com>
2025-09-30 16:46:19 +0100

    Weather Observation Station 9

---
+++ solutions/Basics Select/WeatherObservationStation9.sql
@@ -0,0 +1,23 @@
+
+/*
+	Query the list of CITY names from STATION that do not start with vowels.
+		Your result cannot contain duplicates.
+
+*/
+
+SET NOCOUNT ON;
+
+
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+
+
+
+SELECT DISTINCT CITY FROM STATION
+    WHERE CITY NOT LIKE '[aeiou]%' 
+
+
+
+go
