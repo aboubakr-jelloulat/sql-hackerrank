a81599ca8ab864d3a8dfa845844c702eeacc45e3
aboubakr-jelloulat <abubakerjelloulat@gmail.com>
2025-10-02 17:34:10 +0100

    Employee Names

---
+++ solutions/Basics Select/EmployeeNames.sql
@@ -0,0 +1,14 @@
+
+/*
+
+Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+
+*/
+
+
+SET NOCOUNT ON;
+
+SELECT name FROM Employee ORDER BY name ASC
+
+go
+
